4ebc646dc6c5149a8caaed1df900693cfa73d907
yash1999-project <yashjaypaidar893@gmail.com>
2024-11-16 03:03:09 +0530

    Modified Project

---
+++ Projects/portfolio/__init__.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, abort
+
+app = Flask(__name__)
+projects = [
+    {
+        "name": "Habit tracking app with Python and MongoDB",
+        "thumb": "img/habit-tracking.png",
+        "hero": "img/habit-tracking-hero.png",
+        "categories": ["python", "web"],
+        "slug": "habit-tracking",
+        "prod": "https://udemy.com",
+    },
+    {
+        "name": "Personal finance tracking app with React",
+        "thumb": "img/personal-finance.png",
+        "hero": "img/personal-finance.png",
+        "categories": ["react", "javascript"],
+        "slug": "personal-finance",
+    },
+    {
+        "name": "REST API Documentation with Postman and Swagger",
+        "thumb": "img/rest-api-docs.png",
+        "hero": "img/rest-api-docs.png",
+        "categories": ["writing"],
+        "slug": "api-docs",
+    },
+]
+
+slug_to_project = {project["slug"]: project for project in projects}
+
+
+@app.route("/")
+def home():
+    return render_template("home.html", projects=projects)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/project/<string:slug>")
+def project(slug):
+    if slug not in slug_to_project:
+        abort(404)
+    return render_template(f"project_{slug}.html", project=slug_to_project[slug])
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
